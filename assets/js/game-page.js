20a8cac0296bbbaa435479f5c2f8c44547ef8a21
Ersan Karimi <ersankarimii311@gmail.com>
2022-09-30 21:08:56 +0800

    Create game-page.js
    This is a main entry for game-page.html. What is done in this file include :
    - Import any module which using in this file.
    - Selection the required html elements in this file.
    - Set event click for each choiceCardElements value

---
+++ assets/js/game-page.js
@@ -0,0 +1,53 @@
+import { useState } from "./modules/useState.js";
+import { getComputerChoice } from "./modules/computerChoice.js";
+import { getWinner } from "./modules/getWinner.js";
+import { getImageSource } from "./modules/getImageSource.js";
+
+// Selection of HTML elements with the DOM
+const playerScoreElement = document.querySelector(".player-score");
+const computerScoreElement = document.querySelector(".computer-score");
+const choiceCardElements = document.querySelectorAll(".choice-card");
+const playerChoiceImageElement = document.querySelector("#player-choice-image");
+const computerChoiceImageElement = document.querySelector(
+  "#computer-choice-image"
+);
+
+let [scores, setScores] = useState({
+  player: 0,
+  computer: 0,
+});
+
+choiceCardElements.forEach((card) => {
+  card.addEventListener("click", () => {
+    const playerChoice = card.dataset.choiceType;
+    const [playerImageSource, playerImageAlt] = getImageSource(playerChoice);
+    const computerChoice = getComputerChoice();
+    const [computerImageSource, computerImageAlt] =
+      getImageSource(computerChoice);
+    let winner;
+
+    setTimeout(() => {
+      winner = getWinner(playerChoice, computerChoice);
+      console.log({ playerChoice, computerChoice, winner });
+      scores = setScores({
+        player: winner === "player" ? scores.player + 1 : scores.player,
+        computer: winner === "computer" ? scores.computer + 1 : scores.computer,
+      });
+
+      // remove class "hidden" from the images elements
+      playerChoiceImageElement.classList.remove("hidden");
+      computerChoiceImageElement.classList.remove("hidden");
+
+      // set the image source and alt attributes for the player and computer image
+      playerChoiceImageElement.src = playerImageSource;
+      playerChoiceImageElement.alt = playerImageAlt;
+
+      computerChoiceImageElement.src = computerImageSource;
+      computerChoiceImageElement.alt = computerImageAlt;
+
+      // update the player and computer score
+      playerScoreElement.innerText = scores.player;
+      computerScoreElement.innerText = scores.computer;
+    }, 1000);
+  });
+});
